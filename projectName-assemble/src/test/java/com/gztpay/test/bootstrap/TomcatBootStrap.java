3a13a26dd18e5c883cc6c38b7b123f7eb383eff2
zhouyinyan <gruelac@126.com>
2015-06-27 16:41:28 +0800

    no message

--- projectName-assemble/src/test/java/com/gztpay/test/bootstrap/TomcatBootStrap.java
+++ projectName-assemble/src/test/java/com/gztpay/test/bootstrap/TomcatBootStrap.java
@@ -10,7 +10,7 @@ import com.gztpay.test.bootstrap.tomcat.TomcatBootstrapHelper;
  * @author jlcon
  * @version $Id: ProjectNameTomcatBootStrap.java,v 2.0 2015-6-23 下午6:12:16 JLCON Exp $
  */
-public class ProjectNameTomcatBootStrap {
+public class TomcatBootStrap {
  
 	public static void main(String[] args) throws Exception {
 		new TomcatBootstrapHelper(8080,false, "dev").start();
