d931a100a4b702c62b8341c9b0eebaa8570ee28b
zhouyinyan <gruelac@126.com>
2015-07-14 13:12:21 +0800

    集成mybatis物理分页插件

--- projectName-test/src/test/java/com/projectname/dal/maps/UserMapperTest.java
+++ projectName-test/src/test/java/com/projectname/dal/maps/UserMapperTest.java
@@ -1,10 +1,17 @@
 package com.projectname.dal.maps;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.gzt.test.base.BaseSpringContextTest;
 import com.test.dal.mybatis.maps.UserMapper;
 import com.test.dal.mybatis.model.User;
+import com.test.dal.mybatis.model.UserExample;
+import org.apache.ibatis.session.RowBounds;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import static org.junit.Assert.*;
 
 /**
  * Created by zyinyan on 2015/6/25.
@@ -23,4 +30,35 @@ public class UserMapperTest extends BaseSpringContextTest {
     }
 
 
+    @Test
+    public void testSelect() throws Exception {
+        UserExample userExample = new UserExample();
+        userExample.or();
+        RowBounds rowBounds = new RowBounds(1,10);
+        userMapper.selectByExampleWithRowbounds(userExample,rowBounds);
+    }
+
+    @Test
+    public void testSelect2() throws Exception {
+        UserExample userExample = new UserExample();
+        userExample.or();
+
+        PageHelper.startPage(1,10);
+        List<User> users = userMapper.selectByExample(userExample);
+        PageInfo<User> page = new PageInfo<User>(users);
+
+        //PageInfo包含了非常全面的分页属性
+        assertEquals(1, page.getPageNum());
+        assertEquals(10, page.getPageSize());
+        assertEquals(1, page.getStartRow());
+        assertEquals(2, page.getEndRow());
+        assertEquals(2, page.getTotal());
+        assertEquals(1, page.getPages());
+        assertEquals(1, page.getFirstPage());
+        assertEquals(1, page.getLastPage());
+        assertEquals(true, page.isIsFirstPage());
+        assertEquals(true, page.isIsLastPage());
+        assertEquals(false, page.isHasPreviousPage());
+        assertEquals(false, page.isHasNextPage());
+    }
 }
